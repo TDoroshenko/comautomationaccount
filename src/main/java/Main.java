ed9840c5aaa86400cdd90bbe5b1885457e542386
TDoroshenko <tetyanadoroshenko@gmail.com>
2019-07-12 15:37:56 +0300

    my commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount(1001, 10.3);
+        bankAccount.debit(15.0);
+        bankAccount.credit(1.1);
+        System.out.println(bankAccount.toString());
+    }
+}
---
+++ src/test/java/com/automation/accounttest/BalanceTest.java
@@ -0,0 +1,4 @@
+package com.automation.accounttest;
+
+public class BalanceTest {
+}
